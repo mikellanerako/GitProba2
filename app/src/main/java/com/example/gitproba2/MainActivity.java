52fe452f5a64e7509a8b2adcfa6a05bca3e8898c
Mikel Ajubita <mikellanerako@gmail.com>
2024-09-05 23:04:37 +0200

    Botoiari Toast bat lotuta

--- app/src/main/java/com/example/gitproba2/MainActivity.java
+++ app/src/main/java/com/example/gitproba2/MainActivity.java
@@ -1,19 +1,32 @@
 package com.example.gitproba2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnKlik;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        btnKlik = findViewById(R.id.btnKlik);
+        btnKlik.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Kaixo", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
